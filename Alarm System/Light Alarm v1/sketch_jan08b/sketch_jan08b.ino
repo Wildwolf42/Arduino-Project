95114727006d50d9ab17ace08744f3c34a5b834c
Josef Ericson <02josef.ericson@gulllakecs.org>
2019-01-08 14:09:39 -0500

    Add flashing light to alarm

--- Alarm System/Light Alarm v1/sketch_jan08b/sketch_jan08b.ino
+++ Alarm System/Light Alarm v1/sketch_jan08b/sketch_jan08b.ino
@@ -1,21 +1,8 @@
-/* HC-SR04 Sensor
-   https://www.dealextreme.com/p/hc-sr04-ultrasonic-sensor-distance-measuring-module-133696
-  
-   This sketch reads a HC-SR04 ultrasonic rangefinder and returns the
-   distance to the closest object in range. To do this, it sends a pulse
-   to the sensor to initiate a reading, then listens for a pulse 
-   to return.  The length of the returning pulse is proportional to 
-   the distance of the object from the sensor.
-     
-   The circuit:
-  * VCC connection of the sensor attached to +5V
-  * GND connection of the sensor attached to ground
-  * TRIG connection of the sensor attached to digital pin 2
-  * ECHO connection of the sensor attached to digital pin 4
-   Original code for Ping))) example was created by David A. Mellis
-   Adapted for HC-SR04 by Tautvidas Sipavicius
-   This example code is in the public domain.
- */
+/*
+Prototype code for an alarm system that detects
+how far away something is, then if it is below a certain
+distance, flashes a light
+*/
 
 
 const int trigPin = 2;
@@ -24,13 +11,27 @@ const int echoPin = 4;
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
+    // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
+// the loop function runs over and over again forever
+void loop() {
+  // Detects distance, then flashes light if it meets if statement
+  distance();
+  if (distance() < 10)
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+     delay(1000);                // wait for a second
+}
+
+  
+  long distance()
 {
   // establish variables for duration of the ping, 
   // and the distance result in inches and centimeters:
-  long duration, inches, cm;
+  long duration, inches;
 
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
@@ -40,7 +41,7 @@ void loop()
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
+  
   // Read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
@@ -49,18 +50,7 @@ void loop()
 
   // convert the time into a distance
   inches = microsecondsToInches(duration);
-  
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.println();
-  
-  delay(100);
-  if (inches < 10) {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-     delay(1000);                // wait for a second
-}
+  return inches;
 }
 
 long microsecondsToInches(long microseconds)
